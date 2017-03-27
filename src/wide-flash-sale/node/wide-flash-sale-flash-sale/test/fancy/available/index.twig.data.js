744f6ca94f70527ae49b6dd72858c4052509c6c7
Eric MORAND <emorand@wideagency.com>
2017-03-27 20:19:38 +0200

    Fix issue #15

--- src/wide-flash-sale/node/wide-flash-sale-flash-sale/test/fancy/available/index.twig.data.js
+++ src/wide-flash-sale/node/wide-flash-sale-flash-sale/test/fancy/available/index.twig.data.js
@@ -1,3 +1,5 @@
+const DrupalAttribute = require('drupal-attribute');
+
 module.exports = function (plugin) {
   let data = require('../../default/index.twig.data')(plugin);
 
@@ -22,11 +24,18 @@ module.exports = function (plugin) {
 
   let loremIpsum = '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eget ultrices augue. Proin metus orci, condimentum imperdiet diam id, congue pulvinar nisl. Etiam dignissim nunc eget magna varius finibus. Etiam luctus velit nunc, sed tempor erat euismod eget. Proin lacus quam, placerat sit amet magna vitae, efficitur dictum dolor. Fusce non ipsum felis. Nullam porta nibh a tellus laoreet, ac imperdiet quam mollis. Nunc vitae auctor ex, vel ornare elit.</p><p>Quisque dignissim dui sem, id posuere leo consequat non.\nIn vehicula vulputate tortor et posuere. Etiam vestibulum ut libero vel rhoncus. Etiam rutrum non augue eu fermentum. Donec at dignissim lectus. Curabitur et dolor mi. Quisque id pharetra libero. Donec id tellus accumsan, ultricies erat sed, dapibus massa. Nunc rutrum metus a sem tempor consectetur. Pellentesque a libero laoreet, venenatis arcu quis, pharetra ex.</p>';
 
+  fixture.label = 'TAG HEUER AUTAVIA 2017 FR';
+  fixture.node.field_flash_sale_background.entity.uri.value = 'tagheuer.tag-int.cross-systems.ch/sites/default/files/2017-03/bg-slider.jpg';
+  fixture.node.field_flash_sale_desc_background.entity.uri.value = 'tagheuer.tag-int.cross-systems.ch/sites/default/files/2017-03/bg-section-05.jpg';
+
   fixture.content.field_flash_sale_subtitle = '<p>Calibre Heuer 02</p><p>Automatic Chronograph</p>';
   fixture.content.field_flash_sale_description = '<p>100 M - 42 MM</p><p>CBE2110.FC8226</p>';
-  fixture.content.field_flash_sale_main_image = '<img src="https://www.preprod.tagheuer.com//sites/default/files/styles/flash_sale_main_image/public/2017-03/TH_Autavia_BK_MM-2.png" width="307" height="550">';
+  fixture.content.field_flash_sale_main_image = '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_main_image/public/2017-03/autavia-1.png?itok=bW3TKxoJ" width="307" height="550">';
   fixture.content.field_flash_sale_hashtag = '#happyautavia';
   fixture.content.field_flash_sale_right_desc = '1500 happy clients';
+  fixture.content.field_flash_sale_footer_title = 'Terms of sale';
+  fixture.content.field_flash_sale_footer_text = loremIpsum;
+  fixture.content.field_flash_sale_video = '<figure class="youtube-container youtube-container--responsive"><iframe src="https://www.youtube.com/embed/ihWK-XuecOI?wmode=opaque" width="100%" height="100%" id="youtube-field-player" class="youtube-field-player" title="Embedded video for TAG HEUER AUTAVIA 2017 FR" frameborder="0" allowfullscreen="">&lt;a href="https://www.youtube.com/embed/ihWK-XuecOI?wmode=opaque"&gt;Embedded video for TAG HEUER AUTAVIA 2017 FR&lt;/a&gt; </iframe></figure>';
 
   let productDescTemplate = require('../../../../../paragraph/flash-sale-description/src/index.twig');
 
@@ -43,7 +52,7 @@ module.exports = function (plugin) {
         field_flash_sale_p_title: 'Lorem ipsum',
         field_flash_sale_p_text: loremIpsum,
         field_flash_sale_p_image: [
-          '<img src="//placehold.it/790x530" width="790" height="530">'
+          '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_description_image/public/2017-03/about-01.jpg?itok=_m2xgthX" width="790" height="530">'
         ]
       }
     }),
@@ -59,11 +68,37 @@ module.exports = function (plugin) {
         field_flash_sale_p_title: 'Lorem ipsum',
         field_flash_sale_p_text: loremIpsum,
         field_flash_sale_p_image: [
-          '<img src="//placehold.it/1920x1080" width="1920" height="1080">'
+          '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_description_image/public/2017-03/about-02.jpg?itok=Wu7qwrI9" width="1920" height="1080">'
         ]
       }
     })
   ].join();
+
+  let closeUpImageTemplate = require('../../../../../field/close-up-image/src/index.twig');
+
+  fixture.content.field_flash_sale_images = closeUpImageTemplate.render(
+    {
+      items: [
+        {
+          attributes: new DrupalAttribute(),
+          content: '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_product_image/public/2017-03/tag-closeup-01.jpg?itok=eAU8WFCR">'
+        },
+        {
+          attributes: new DrupalAttribute(),
+          content: '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_product_image/public/2017-03/tag-closeup-02.jpg?itok=9xnc78qr">'
+        },
+        {
+          attributes: new DrupalAttribute(),
+          content: '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_product_image/public/2017-03/tag-closeup-03.jpg?itok=S4N2inp7">'
+        },
+        {
+          attributes: new DrupalAttribute(),
+          content: '<img src="http://tagheuer.tag-int.cross-systems.ch/sites/default/files/styles/flash_sale_product_image/public/2017-03/tag-closeup-04.jpg?itok=G0XFTnIy">'
+        }
+      ],
+      field_name: 'content.field_flash_sale_images'
+    }
+  );
 
   return {
     demo: data.demo,
