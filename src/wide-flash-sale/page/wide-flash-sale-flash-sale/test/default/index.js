a4381b963d687f8c491e816afbb821c81abbfb39
Eric MORAND <emorand@wideagency.com>
2017-03-28 11:43:44 +0200

    Fix Redmine issue #25278

--- src/wide-flash-sale/page/wide-flash-sale-flash-sale/test/default/index.js
+++ src/wide-flash-sale/page/wide-flash-sale-flash-sale/test/default/index.js
@@ -1,2 +1,38 @@
 require('../../src');
 require('../../../../node/wide-flash-sale-flash-sale/src');
+
+let mockjax = require('jquery-mockjax')(require('jquery'), global);
+
+mockjax({
+  url: '/ws/flashsale/get-sale-config',
+  type: 'get',
+  dataType: 'json',
+  responseText: {
+    config: {
+      country: 'uk',
+      language: 'en-gb'
+    }
+  }
+});
+
+mockjax({
+  url: '/ws/flashsale/get-price',
+  type: 'get',
+  dataType: 'json',
+  responseText: {
+    price: {
+      price: '1234.56',
+      availability: 'AVAILABLE'
+    }
+  }
+});
+
+
+mockjax({
+  url: '/ws/flashsale/set-sale-config',
+  type: 'post',
+  dataType: 'json',
+  responseText: {
+    redire: '#'
+  }
+});
