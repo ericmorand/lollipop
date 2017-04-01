ec12aec3d7a1f909beb80b67a48a5cb9cad86a70
Eric MORAND <eric.morand@gmail.com>
2017-04-01 18:07:31 +0200

    WIP

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const log = require('log-util');
 const merge = require('merge');
 const path = require('path');
 
+const chokidar = require('chokidar');
 const Stromboli = require('stromboli');
 const ComponentsBuilder = require('./lib/components-builder');
 const StyleguideBuilder = require('./lib/styleguide-builder');
@@ -14,59 +15,19 @@ const BrowserSync = require('browser-sync');
 
 const demoBuilderConfig = require('./config/demo');
 
-class MasterBuilder extends ComponentsBuilder {
-
-}
-
 if (cluster.isMaster) {
   console.log(`Master ${process.pid} is running`);
 
-  let masterBuilder = new MasterBuilder();
+  let builder = new ComponentsBuilder();
+  let watchers = new Map();
 
-  masterBuilder.start(demoBuilderConfig).then(
-    function (result) {
-      console.log(result);
-
-      components.forEach(function (component) {
-        cluster.fork({
-          component: JSON.stringify(component)
-        });
-      });
-    }
-  );
-
-  masterBuilder.pluginRenderComponentDidEnd = function (worker, plugin, component, binaries, dependencies) {
-    let self = this;
-
-    // watch dependencies
-    let watcher = null;
-
-    if (!self.componentsWatchers.has(component.name)) {
-      self.componentsWatchers.set(component.name, new Map());
-    }
-
-    let componentWatchers = self.componentsWatchers.get(component.name);
-
-    self.info('WATCHER WILL WATCH', dependencies, 'USING PLUGIN', plugin.name);
-
-    watcher = self.getWatcher(dependencies, function () {
-      self.info('WATCHER FOR COMPONENT', component.name, 'AND PLUGIN', plugin.name, 'WILL BE CLOSED');
-
-      this.close();
-
-      worker.send({
-        type: 'renderComponent',
-        plugin: plugin.name
-      })
-    });
-
-    componentWatchers.set(plugin.name, watcher);
-
-    // reload Browsersync
-    let bs = BrowserSync.get(component.name);
+  let reload = function (component) {
+    let bs = BrowserSync.has(component.name) ? BrowserSync.get(component.name) : null;
 
     if (bs) {
-      binaries.forEach(function (binary) {
+      let renderResult = component.renderResult;
+
+      renderResult.binaries.forEach(function (binary) {
         if (path.extname(binary) !== '.map') {
           bs.reload(binary);
         }
@@ -74,38 +35,178 @@ if (cluster.isMaster) {
     }
   };
 
+  let closeWatcher = function (component, worker) {
+    let watcher = null;
+
+    if (watchers.has(component.name)) {
+      let componentWatchers = watchers.get(component.name);
+
+      if (componentWatchers.has(worker.id)) {
+        watcher = componentWatchers.get(worker.id);
+
+        return watcher.close();
+      }
+    }
+
+    return Promise.resolve(true);
+  };
+
+  let watch = function (component, files, worker) {
+    builder.info('WATCHER WILL WATCH', files, 'USING WORKER', worker.id);
+
+    let watcher = chokidar.watch(files, demoBuilderConfig.chokidar).on('all', function (type, file) {
+      builder.info(file, type);
+
+      if (file === '/home/ericmorand/Projects/lollipop/src/wide-flash-sale/node/wide-flash-sale-flash-sale/src/index.scss') {
+        builder.warn('>>> WATCHER WILL SEND MESSAGE TO WORKER', worker.id);
+      }
+
+      worker.send({
+        type: 'test'
+      });
+    });
+
+    // store the watcher
+    if (!watchers.has(component.name)) {
+      watchers.set(component.name, new Map());
+    }
+
+    let componentWatchers = watchers.get(component.name);
+
+    componentWatchers.set(worker.id, watcher);
+
+    return watcher;
+  };
+
+  let h = function (component, plugin, worker) {
+    // close watcher
+    return closeWatcher(component, worker).then(
+      function () {
+        // reload BS
+        reload(component);
+
+        // watch again
+        let renderResult = component.renderResult;
+        let files = renderResult.sourceDependencies.concat(renderResult.binaryDependencies);
+
+        watch(component, files, worker);
+      }
+    );
+  };
+
+  builder.start(demoBuilderConfig).then(
+    function (components) {
+      let self = builder;
+
+      builder.warn('== START DONE');
+
+      // browser-sync
+      let bsPromise = new Promise(function (fulfill, reject) {
+        let processComponentAtIndex = function (index) {
+          let component = components[index];
+          let browserSync = require('browser-sync').create(component.name);
+          let browserSyncConfig = merge({}, self.config.browserSync);
+
+          browserSyncConfig.server = browserSyncConfig.server + '/' + component.name;
+
+          browserSync.init(browserSyncConfig, function (err, bs) {
+            // component.url = '/html';
+            // component.port = bs.options.get('port');
+
+            index++;
+
+            if (index < components.length) {
+              processComponentAtIndex(index);
+            }
+            else {
+              fulfill(components);
+            }
+          });
+        };
+
+        processComponentAtIndex(0);
+      });
+
+      bsPromise.then(
+        function () {
+          components.forEach(function (component) {
+            let renderResults = component.renderResults;
+
+            renderResults.forEach(function (renderResult, pluginName) {
+              // fork
+              let worker = cluster.fork({
+                component: JSON.stringify({
+                  name: component.name,
+                  path: component.path
+                }),
+                plugin: JSON.stringify({
+                  name: pluginName
+                })
+              });
+
+              // watch
+              let files = renderResult.sourceDependencies.concat(renderResult.binaryDependencies);
+
+              watch(component, files, worker);
+            });
+          });
+        }
+      );
+    }
+  );
+
   cluster.on('message', function (worker, message, handle) {
     if (message.type === 'pluginRenderComponent') {
-      masterBuilder.pluginRenderComponentDidEnd(worker, message.plugin, message.component, message.binaries, message.dependencies);
+      h(message.component, message.plugin, worker);
     }
   });
 
   cluster.on('exit', function (worker, code, signal) {
     console.log(`worker ${worker.process.pid} died`);
   });
-} else {
+}
+else {
   let component = JSON.parse(process.env.component);
+  let plugin = JSON.parse(process.env.plugin);
+  let builder = new ComponentsBuilder();
 
-  console.log(`Worker ${process.pid} started ${component.name}`);
+  builder.warn(`Worker ${process.pid} started ${component.name}::${plugin.name}`);
 
-  let workerBuilder = new ComponentsBuilder();
-  let pluginRenderComponent = workerBuilder.pluginRenderComponent;
+  let pluginName = plugin.name;
 
-  workerBuilder.pluginRenderComponent = function (plugin, component) {
-    return pluginRenderComponent.call(workerBuilder, plugin, component).then(
-      function (result) {
-        process.send({
-          type: 'pluginRenderComponent',
-          component: {
-            name: result.component.name
-          },
-          plugin: {
-            name: plugin.name
-          },
-          binaries: result.binaries,
-          dependencies: result.dependencies
+  component.renderResults = new Map();
+
+  builder.getPlugins(demoBuilderConfig).then(
+    function (plugins) {
+      let plugin = plugins.find(function (item) {
+        return (item.name === pluginName);
+      });
+
+      if (plugin) {
+        process.on('message', function (worker, message, handle) {
+          console.log(process.pid, 'did receive', message);
+
+          builder.pluginRenderComponent(plugin, component).then(
+            function (component) {
+              let renderResult = component.renderResults.get(plugin.name);
+
+              process.send({
+                type: 'pluginRenderComponent',
+                component: {
+                  name: component.name,
+                  renderResult: renderResult
+                },
+                plugin: {
+                  name: plugin.name
+                }
+              });
+            }
+          );
         });
       }
-    );
-  };
+    },
+    function(err) {
+      console.log(err);
+    }
+  );
 }
